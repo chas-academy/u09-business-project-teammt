48df22124b46bf2ecee25a8f3ac35b6a69cd88d7
memoona.tahira <memoona.tahira@gmail.com>
2025-06-07 21:36:44 +0200

    allow all origin

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,19 @@ connectDB();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Allow all origins
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  
+  // Handle preflight requests
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
 app.get('/', (req, res) => {
   res.send('heloo express!');
 });
